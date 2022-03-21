421258e92a61759702a8f363f74a58e227bcea08
Rodion Sibov <rodionsibov@gmail.com>
2022-03-21 23:44:23 +0200

    update

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  users = [
+    {
+      id: '1',
+      name: 'Jack',
+      age: 21
+    },
+    {
+      id: '2',
+      name: 'John',
+      age: 28
+    },
+    {
+      id: '3',
+      name: 'Sam',
+      age: 58
+    }
+  ]
+
+  // arr = Array.from({ length: 10 }, (_, i) => i + 1)
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  removeUser(id: string): void {
+    this.users = this.users.filter(user => user.id !== id)
+    console.log('removeUser', id);
+
+  }
+
+  addUser(user: string): void {
+    if (user) {
+      this.users.unshift({
+        id: (this.users.length + 1).toString(),
+        name: user,
+        age: 33
+      })
+    }
   }
 
 }
