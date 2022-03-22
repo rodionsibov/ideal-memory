c22f52654f7695e1337633f3fabd51c7923e0f2e
Rodion Sibov <rodionsibov@gmail.com>
2022-03-22 23:22:52 +0200

    first commit

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+year: string = ''
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.year = new Date().getFullYear().toString()
+  }
+
+}
