9ef96032a05e1c36e43a852756198a1201f2a0ef
Rodion Sibov <rodionsibov@gmail.com>
2022-03-29 21:44:44 +0300

    first commit

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form!: FormGroup
+  
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+  
+  initializeForm() {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  
+
+}
