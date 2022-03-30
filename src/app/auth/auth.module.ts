8999a6d5f95b53d2f340f3fb0571800739277914
Rodion Sibov <rodionsibov@gmail.com>
2022-03-30 17:29:37 +0300

    update

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,4 +1,3 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
@@ -9,19 +8,12 @@ import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [
   {
     path: 'register',
-    component: RegisterComponent
+    component: RegisterComponent,
   },
-]
+];
 
 @NgModule({
-  declarations: [
-    RegisterComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    ReactiveFormsModule,
-  ]
+  declarations: [RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
 })
-export class AuthModule {
-}
+export class AuthModule {}
--- src/app/shared/types/current-user.ts
+++ src/app/shared/types/current-user.ts
@@ -1,10 +1,10 @@
 export interface CurrentUser {
-    id: number,
-    email: string,
-    createdAt: string,
-    updatedAt: string,
-    username: string,
-    bio: string | null,
-    image: string | null,
-    token: string
+  id: number;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  username: string;
+  bio: string | null;
+  image: string | null;
+  token: string;
 }
